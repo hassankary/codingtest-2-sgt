68d124aae81ca15ae992c8d9a2f0c6e91d3983eb
Hassan Kaeru <hassanaskary29@gmail.com>
2024-10-04 04:45:43 +0700

    update: data & section

--- src/app/data/data.js
+++ src/app/data/data.js
@@ -53,6 +53,9 @@ export const dataFourthSection = [
   "The right to life",
   "4 paws",
   "The kingdom of cats",
+  "Zvir",
+  "Mercy",
+  "Happy dog",
   "Give me a paw, friend",
   "Friend",
   "Hope",
@@ -67,4 +70,149 @@ export const dataFourthSection = [
   "Second chance",
   "SOS",
   "Best friends",
+];
+
+export const dataFifthSection = [
+  {
+    name: "Daryna Deriy",
+    description: "Chairman of the Board",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/62744ee9fce7fbaf400c22f8_2-p-500.jpeg",
+  },
+  {
+    name: "Polina Snisarenko-Kulchytska",
+    description: "Curator of the Foundation",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/62744edaac91b4183ac27e10_5-p-500.jpeg",
+  },
+  {
+    name: "Bohdan Kulchytsky",
+    description: "Executive Manager",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/62744ec97586a996f48f7a85_1-p-500.jpeg",
+  },
+  {
+    name: "Mykhaylo Deriy",
+    description: "Operation Manager",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/62744eae22e10d8a15f909ef_9-p-500.jpeg",
+  },
+  {
+    name: "Ksenia Nikishina",
+    description: "Office manager",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/62744e8e05f6c22bac85252a_7.jpg",
+  },
+  {
+    name: "Anna Andriychuk",
+    description: "SMM-manager",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/62744f0eb7cd3462ef475ebf_6-p-500.jpeg",
+  },
+  {
+    name: "Anastasia Kovalchuk",
+    description: "Director",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/62744e6761e425c6956af3a5_3-p-500.jpeg",
+  },
+  {
+    name: "Basil Gloo",
+    description: "Web Developer",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/626ae0bcd92bf48017c14083_basil-square.png",
+  },
+  {
+    name: "Lina Yakobchuk",
+    description: "Designer",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/626ae01da2a805d176b1606b_%D0%AF%D0%BA%D0%BE%D0%B1%D1%87%D1%83%D0%BA-p-500.jpeg",
+  },
+  {
+    name: "Anastasia Yevchenko",
+    description: "Translator",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fc796cf3602b53a7512cf_4-p-500.jpeg",
+  },
+  {
+    name: "Office Manager",
+    description: "open vacancy",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fc7fcdc0f8e9cbcc8ffed_isa-logo-dog-clean.svg",
+  },
+  {
+    name: "SEO specialist",
+    description: "open vacancy",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fc81139e6f5dca2d02054_isa-logo-cat-clean.svg",
+  },
+];
+
+export const dataSixthSection = [
+  {
+    name: "Oleksiy Tritenko",
+    description: "theater and film actor",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/628398012a964488bb09078d_13-p-500.jpeg",
+  },
+  {
+    name: "Stanislav Boklan",
+    description: "People's Artist of Ukraine",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/628397b3f06d2066cfe8e9a2_14-p-500.jpeg",
+  },
+  {
+    name: "Alexander Rudinsky",
+    description: "theater and film actor",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fcdfcb2137dcf7db53e7f_12-p-500.jpeg",
+  },
+  {
+    name: "Oksana Zhdanova",
+    description: "theater and film actress",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fcdd72d729d8ecacf6de7_11-p-500.jpeg",
+  },
+  {
+    name: "Natalka Denisenko",
+    description: "theater and film actress",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fcdaedc0f8eea17c92429_10-p-500.jpeg",
+  },
+  {
+    name: "Maksym Samchyk",
+    description: "theater and film actor",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fcd87d51b9051dfcbf349_09-p-500.jpeg",
+  },
+  {
+    name: "Kateryna Vyshneva",
+    description: "theater and film actress",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fcd5ca9508410b3a0d824_08-p-500.jpeg",
+  },
+  {
+    name: "Irma Vitovska",
+    description: "Honored Actress of Ukraine",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fcd3288758547e6ee0e22_07-p-500.jpeg",
+  },
+  {
+    name: "Igor Portyanko",
+    description: "actor of theater and cinema",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fcd1790c298c49bc7c436_06-p-500.jpeg",
+  },
+  {
+    name: "Dmytro Surzhikov",
+    description: "Honored Artist of Ukraine",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fccf0fc672a5dcfa87ee9_05-p-500.jpeg",
+  },
+  {
+    name: "Daria Barihashvili",
+    description: "theater and film actress",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fccc57c3cb5177369015e_04-p-500.jpeg",
+  },
+  {
+    name: "Vera Kobzar",
+    description: "theater and film actress",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fcc9bc50d7acc036982a9_03-p-500.jpeg",
+  },
+  {
+    name: "Andriy Fedynchyk",
+    description: "theater and film actor",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fcc7339e6f5389ed03013_02-p-500.jpeg",
+  },
+  {
+    name: "Anastasia Nesterenko",
+    description: "theater and film actress",
+    src: "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/627fcc4b694dfd4d65a13230_01-p-500.jpeg",
+  },
+];
+
+export const dataSeventhSection = [
+  "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/62aa3401042d425391235aaa_Logo-adviRES-p-500.jpeg",
+  "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/62aa33e1380de680b7110ebf_ovid-wind-logo.png",
+  "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/6265d803f32677c8352bc485_tbs_logo1-02-p-500.png",
+  "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/6265d7edf0c46ce38f559328_download.png",
+  "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/6265d7d930a5c2377aedf9d4_logo_laudis_accountancy-p-500.png",
+  "https://cdn.prod.website-files.com/6264607e95d7be676470ea8d/6265d7c51ad96b1cbf31b0c0_photo_2022-04-21%2020.43.09-p-500.jpeg",
 ];
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,22 @@
 import Image from "next/image";
-import { logoFirstSection } from "./data/data";
+import {
+  dataFifthSection,
+  dataFourthSection,
+  dataSeventhSection,
+  dataSixthSection,
+  dataThirdSection,
+  logoFirstSection,
+} from "./data/data";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <div className="h-screen flex justify-center items-center px-[10%] bg-[#FCF944]">
-        <button className="fixed top-7 right-7 md:top-[42px] lg:top-7 lg:right-7 md:right-10 px-[7px] py-[3.5px] text-black text-[4vw] xs:text-[2.75vw] md:text-[2.5vw] lg:text-[2vw] bg-white border-[3.2px] border-black">
-          <h1>donate</h1>
+        <button className="z-10 fixed top-7 right-7 md:top-[42px] lg:top-7 lg:right-7 md:right-10 text-black text-[4vw] xs:text-[2.75vw] md:text-[2.5vw] lg:text-[2vw] bg-white">
+          <h1 className="px-[7px] py-[3.5px] border-[3.2px] border-black hover:translate-x-2.5 hover:-translate-y-2.5 transition-all duration-300 ease-in-out">
+            <span>donate</span>
+          </h1>
         </button>
         <div className="flex flex-col space-y-[16px] xs:space-y-[32px] text-black ">
           <div className="flex justify-center space-x-[6%]">
@@ -54,14 +64,16 @@ export default function Home() {
       </div>
       <div className="flex justify-center items-center p-[10%] text-black bg-white">
         <div className="w-full ">
-          <h1 className=" font-semibold text-[4vw]">We are</h1>
-          <p className="text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
+          <h1 className="font-semibold text-[8vw] xs:text-[4vw]">We are</h1>
+          <p className="text-[3.75vw] xs:text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
             team of like-minded people, socially active companies, media
             personalities, volunteers who help low-income shelters and foster
             carers to support homeless animals.
           </p>
-          <h1 className=" font-semibold text-[4vw]">Our primary goal</h1>
-          <p className="text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
+          <h1 className="font-semibold text-[8vw] xs:text-[4vw]">
+            Our primary goal
+          </h1>
+          <p className="text-[3.75vw] xs:text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
             is to provide animals with the most necessary things - food,
             medicine, and urgent needs for the livelihood of shelters. We
             believe in our power and the power of conscious citizens who care
@@ -72,11 +84,11 @@ export default function Home() {
             lives. We all know the healing power of warmth, grace, and big
             loving eyes.
           </p>
-          <div className="ml-4 xs:ml-8 lg:ml-16 p-4 text-[2.5vw] italic border-[#ff97d0] border-l-4">
-            {`"Animals need to have friends. Just like humans." - James Herriot,
-            All Creatures Great and Small`}
+          <div className="ml-4 xs:ml-8 lg:ml-16 p-4 text-[3.75vw] xs:text-[2.5vw] italic border-[#ff97d0] border-l-4">
+            &quot;Animals need to have friends. Just like humans.&quot; <br /> -
+            James Herriot, All Creatures Great and Small
           </div>
-          <p className="text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
+          <p className="text-[3.75vw] xs:text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
             Who else if not we should support our younger brothers, especially
             in difficult times? Let&apos;s be friends!
           </p>
@@ -88,6 +100,219 @@ export default function Home() {
               className="absolute top-0 left-0 w-full h-full"
               src="https://www.youtube.com/embed/5Wk1rp99B7o?wmode=opaque&widget_referrer=https%3A%2F%2Fisa-charity.webflow.io%2F&enablejsapi=1&origin=https%3A%2F%2Fcdn.embedly.com&widgetid=1"
               allowFullScreen
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center p-[10%] text-white bg-[#3E88FF]">
+        <div className="w-full">
+          <h1 className="font-semibold text-[8vw] xs:text-[4vw]">
+            Our projects
+          </h1>
+          <p className="text-[3.75vw] xs:text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
+            are very different in terms of priority, scale and complexity of
+            implementation.
+          </p>
+          <div className="mt-7 lg:mt-[4em] grid grid-cols-1 gap-y-7">
+            {dataThirdSection?.map((d) => {
+              return (
+                <Link
+                  href={"/"}
+                  key={d.title}
+                  className="bg-black even:bg-[#31BE32] last:bg-[#FF97D0]"
+                >
+                  <div className="flex flex-col px-[42px] py-7 xs:p-[56px] space-y-[0.5em] border-[3.2px] border-white hover:translate-x-3.5 hover:-translate-y-3.5 transition-all duration-200">
+                    <h1 className="font-semibold text-[6vw] xs:text-[3.5vw] lg:text-[3vw]">
+                      {d.title}
+                    </h1>
+                    <p className="text-[3.75vw] xs:text-[2.5vw] lg:text-[2vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
+                      {d.subtitle}
+                    </p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center p-[10%] text-black bg-[#fcf944]">
+        <div className="w-full">
+          <h1 className="font-semibold text-[8vw] xs:text-[4vw]">
+            We want to help
+          </h1>
+          <p className="text-[3.75vw] xs:text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
+            homeless packs, volunteers, guardians, overstayers, shelters and
+            other organisations
+          </p>
+          <div className="mt-[28px] lg:mt-[56px] grid grid-cols-2 lg:grid-cols-3 gap-7 xs:gap-y-[42px] xs:gap-x-[4%] lg:gap-x-[3.5%]">
+            {dataFourthSection?.map((d) => {
+              return (
+                <Link href={"/"} key={d} className="bg-white">
+                  <div className="h-full flex flex-col items-center justify-center p-[28px] text-center space-y-[0.5em] border-[3.2px] border-black hover:translate-x-3.5 hover:-translate-y-3.5 transition-all duration-300 ease-in-out">
+                    <h1 className=" text-[3.5vw] xs:text-[2.5vw] lg:text-[1.5vw]">
+                      {d}
+                    </h1>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+          <Link href={"#"}>
+            <div className="w-fit mt-[10%] bg-[#FF97D0]">
+              <div className="group flex items-center justify-center space-x-4 px-8 py-4 text-center border-[3.2px] border-black">
+                <span className="font-bold text-[3.5vw] xs:text-[2vw] lg:text-[1.75vw]">
+                  apply for help
+                </span>
+                <Image
+                  src={
+                    "https://cdn.prod.website-files.com/62640d8cb86529032eccb9d1/6273a272fe3407579aa3d6c9_PngItem_344959-p-500.png"
+                  }
+                  alt="Arrow Icon"
+                  height={100}
+                  width={100}
+                  unoptimized
+                  className="max-h-[30px] max-w-[30px] md:max-h-[40px] md:max-w-[40px] lg:max-h-[50px] lg:max-w-[50px] flex object-scale-down rotate-180 group-hover:translate-x-3 transition-all duration-200 ease-in-out"
+                />
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
+      <div className="flex justify-center items-center p-[10%] text-black bg-white">
+        <div className="w-full">
+          <h1 className="font-semibold text-[8vw] xs:text-[4vw]">Our team</h1>
+          <p className="text-[3.75vw] xs:text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
+            consists of completely different people who are united by a common
+            desire - to help
+          </p>
+          <div className="mt-[28px] lg:mt-[56px] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-[10%] md:gap-x-[4%] gap-y-7">
+            {dataFifthSection?.map((d) => {
+              return (
+                <Link href={"/"} key={d.name} className="bg-[#fcf944]">
+                  <div className="h-full flex flex-col items-center justify-start p-[14px] xs:p-7 text-center border-[3.2px] border-black hover:scale-90 transition-all duration-300 ease-in-out">
+                    <Image
+                      src={d.src}
+                      alt={`${d.name} - Image`}
+                      height={350}
+                      width={350}
+                      unoptimized
+                      className="mb-[14px] xs:mb-[28px] grayscale"
+                    />
+                    <h1 className=" mb-[0.5em] text-[3.5vw] xs:text-[3vw] md:text-[2.5vw] lg:text-[1.5vw] leading-[1.25em]">
+                      {d.name}
+                    </h1>
+                    <p className="text-[3vw] xs:text-[2.5vw] sm:text-[2vw] lg:text-[1.25vw]">
+                      {d.description}
+                    </p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center p-[10%] text-black bg-[#FF97D0]">
+        <div className="w-full">
+          <h1 className="font-semibold text-[8vw] xs:text-[4vw]">
+            Socially active citizens
+          </h1>
+          <p className="text-[3.75vw] xs:text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
+            support us
+          </p>
+          <div className="mt-[28px] lg:mt-[56px] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-[28px] lg:gap-x-[4%] gap-y-7 xs:gap-y-[42px] lg:gap-y-[70px]">
+            {dataSixthSection?.map((d) => {
+              return (
+                <Link href={"#"} key={d.name}>
+                  <div className="h-full flex flex-col items-center justify-start text-center hover:translate-x-3.5 hover:-translate-y-3.5 transition-all duration-300 ease-in-out">
+                    <Image
+                      src={d.src}
+                      alt={`${d.name} - Image`}
+                      height={350}
+                      width={350}
+                      unoptimized
+                      className="mb-[16px] grayscale"
+                    />
+                    <h1 className=" mb-[0.5em] text-[3.5vw] xs:text-[3vw] md:text-[2.5vw] lg:text-[1.5vw] leading-[1.25em]">
+                      {d.name}
+                    </h1>
+                    <p className="text-[3vw] xs:text-[2.5vw] sm:text-[2vw] lg:text-[1.25vw]">
+                      {d.description}
+                    </p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center p-[10%] text-black bg-white">
+        <div className="w-full">
+          <h1 className="font-semibold text-[8vw] xs:text-[4vw]">
+            Our partners
+          </h1>
+          <p className="text-[3.75vw] xs:text-[2.5vw] leading-[1.5em] mt-[0.5em] mb-[1em]">
+            are take care of our fund and help us with many questions
+          </p>
+          <div className="mt-[28px] lg:mt-[56px] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-[28px] lg:gap-x-[4%] gap-y-7 xs:gap-y-[42px] lg:gap-y-[70px]">
+            {dataSeventhSection?.map((d, i) => {
+              return (
+                <Link href={"#"} key={d}>
+                  <div className="h-full flex flex-col items-center justify-center text-center hover:translate-x-3.5 hover:-translate-y-3.5 transition-all duration-300 ease-in-out">
+                    <Image
+                      src={d}
+                      alt={`Partner ${i} - Image`}
+                      height={350}
+                      width={350}
+                      unoptimized
+                      className="grayscale"
+                    />
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center p-[10%] text-black bg-[#31BE32]">
+        <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-[28px] lg:gap-0">
+          <div className="w-full">
+            <h1 className="font-semibold text-[8vw] xs:text-[4vw]">
+              Contact us
+            </h1>
+            <p className="text-[3.75vw] xs:text-[2.5vw] leading-[1.5em]">
+              Kyiv, Ukraine
+            </p>
+            <p className="text-[3.75vw] xs:text-[2.5vw] leading-[1.5em]">
+              mail@isa.co.ua
+            </p>
+            <div className="w-fit flex justify-start my-8 ">
+              {logoFirstSection?.map((d) => {
+                return (
+                  <div
+                    key={d.src}
+                    className="flex justify-center h-12 w-12 md:h-14 md:w-14 lg:h-16 lg:w-16 p-2"
+                  >
+                    <Image
+                      src={d.src}
+                      alt={d.alt}
+                      height={48}
+                      width={48}
+                      unoptimized
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="w-1/2 lg:w-full">
+            <Image
+              src={
+                "https://cdn.prod.website-files.com/62640d8cb86529032eccb9d1/62647f9fbe07235ba9b28d67_star-white-with-animals.svg"
+              }
+              alt={"Image - Footer"}
+              height={700}
+              width={700}
+              unoptimized
             />
           </div>
         </div>
